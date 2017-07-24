1e5425bb1244be898e0ac173e264789eb95f731b
woo85246 <woo85246@naver.com>
2017-07-24 17:32:57 +0900

    gitBasicSetting

---
+++ src/main/java/com/woohyun/board/BoardApplication.java
@@ -0,0 +1,13 @@
+package com.woohyun.board;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@SpringBootApplication
+public class BoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoardApplication.class, args);
+	}
+}
